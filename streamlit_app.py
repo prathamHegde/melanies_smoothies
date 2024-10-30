9aaaad82e371376c500b7fa760641c5515eed9fb
Pratham Hegde <114552680+prathamHegde@users.noreply.github.com>
2024-10-30 23:35:51 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,7 +33,7 @@ if ingredients_list:
         ingredients_string += fruit_chosen + ' '
         st.subheader(fruit_chosen + ' Nutrition Information')
        # fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ fruit_chosen)
-        fv_df = st.dataframe(data=fruityvice_response.json(),use_conatiner_width=True)
+        #fv_df = st.dataframe(data=fruityvice_response.json(),use_conatiner_width=True)
 
     #st.write(ingredients_string)
 
