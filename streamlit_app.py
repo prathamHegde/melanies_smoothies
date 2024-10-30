4b4416edc4b530d4b64585854ba80b3e61ac16bd
Pratham Hegde <114552680+prathamHegde@users.noreply.github.com>
2024-10-30 22:36:24 +0530

    Update streamlit_app.py
    Added api part

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,3 +45,7 @@ if ingredients_list:
         session.sql(my_insert_stmt).collect()
         st.success(f"✅ Your smoothie is ordered, {name_on_order}.")
         #st.success('Your Smoothie is ordered,',icon='✅')
+
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+st.text(fruityvice_response)
